19736586f23aa0441b25294695c002ad80a356c4
ElviraKonovalov <konovalovelvira@gmail.com>
2021-04-21 20:54:06 -0400

    Q1 to Q5 done with working varations

--- rasa/actions/actions.py
+++ rasa/actions/actions.py
@@ -223,3 +223,142 @@ class WhichCourseAtUniTeachTopic(Action):
             print(answer)
 
         return []
+
+
+# Q4) Which department offers the course [course]?
+class WhichDepOffersCourse(Action):
+
+    def name(self) -> Text:
+        return "action_dep_offer_course"
+
+    def response_request(self, csubject, cnumber):
+        response = requests.post("http://localhost:3030/acad/sparql",
+                                 data={'query': """
+                            PREFIX vivo: <http://vivoweb.org/ontology/core#> 
+                            PREFIX rdf: <http://www.w3.org/1999/02/22-rdf-syntax-ns#>
+                            PREFIX DC: <http://purl.org/dc/terms/> 
+                            PREFIX acad: <http://acad.io/schema#> 
+                            PREFIX foaf: <http://xmlns.com/foaf/0.1/> 
+                            PREFIX rdfs: <http://www.w3.org/2000/01/rdf-schema#> 
+                            PREFIX xsd: <http://www.w3.org/2001/XMLSchema#> 
+                            PREFIX acaddata: <http://acad.io/data#>
+
+                            SELECT ?cname ?cdepartment
+                            WHERE{
+                            ?course a vivo:Course.
+                            ?course foaf:name ?cname.
+                            ?course acad:courseNumber "%s"^^xsd:int.
+                            ?course acad:courseSubject "%s"^^xsd:string.
+                            ?course vivo:AcademicDepartment ?cdepartment.
+                            }
+                            """ % (cnumber, csubject)
+                                       })
+        # Use the json module to load CKAN's response into a dictionary.
+
+        y = json.loads(response.text)
+
+        # the result is a Python dictionary:
+        results = y["results"]
+
+        departments = []
+
+        for result in results["bindings"]:
+            cdepartment = result["cdepartment"]
+            dep = cdepartment["value"]
+            departments.append(dep)
+
+        return departments
+
+    def run(self, dispatcher: CollectingDispatcher,
+            tracker: Tracker,
+            domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:
+
+        course = tracker.slots['course'].replace(" ", "")
+
+        # print(course)
+
+        values = re.split(r'([^\d]*)(\d.*)', course, maxsplit=1)
+        print(values)
+
+        csubject = values[1].upper()
+        cnumber = values[2]
+
+        departments = self.response_request(csubject, cnumber)
+
+        if not departments:
+            print(f"The course {course} is not offered in any departments.")
+        else:
+            answer = f"The course {course} is offered in the department:\n"
+            for dep in departments:
+                answer = answer + "- " + dep + "\n"
+            print(answer)
+
+        return []
+
+
+# Q5) What content does [course] [lecture] consist of?
+class ContentCourseLecture(Action):
+
+    def name(self) -> Text:
+        return "action_content_course_lecture"
+
+    def response_request(self, course, lnumber):
+        # print(course)
+        # print(lnumber)
+        response = requests.post("http://localhost:3030/acad/sparql",
+                                 data={'query': """
+                            PREFIX vivo: <http://vivoweb.org/ontology/core#> 
+                            PREFIX rdf: <http://www.w3.org/1999/02/22-rdf-syntax-ns#>
+                            PREFIX DC: <http://purl.org/dc/terms/> 
+                            PREFIX acad: <http://acad.io/schema#> 
+                            PREFIX foaf: <http://xmlns.com/foaf/0.1/> 
+                            PREFIX rdfs: <http://www.w3.org/2000/01/rdf-schema#> 
+                            PREFIX xsd: <http://www.w3.org/2001/XMLSchema#> 
+                            PREFIX acaddata: <http://acad.io/data#>
+
+                            SELECT ?typeLabel
+                            WHERE{
+                            acaddata:%s-LEC%s acad:hasContent ?content.
+                            ?content a ?type.
+                            ?type rdfs:label ?typeLabel.
+                            }
+                            """ % (course, lnumber)
+                                       })
+        # Use the json module to load CKAN's response into a dictionary.
+
+        y = json.loads(response.text)
+
+        # the result is a Python dictionary:
+        results = y["results"]
+
+        content = []
+
+        for result in results["bindings"]:
+            typeLabel = result["typeLabel"]
+            con = typeLabel["value"]
+            content.append(con)
+
+        return content
+
+    def run(self, dispatcher: CollectingDispatcher,
+            tracker: Tracker,
+            domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:
+
+        course = tracker.slots['course'].upper().replace(" ", "")
+        lecture = tracker.slots['lecture'].replace(" ", "")
+
+        values = re.split(r'([^\d]*)(\d.*)', lecture, maxsplit=1)
+
+        lnumber = values[2]
+
+        content = self.response_request(course, lnumber)
+
+        if not content:
+            print(f"The course {course} lecture{lnumber} does not have any content.")
+        else:
+            answer = f"The course {course} lecture{lnumber} consists of the following content:\n"
+            for con in content:
+                answer = answer + "- " + con + "\n"
+            print(answer)
+
+        return []
